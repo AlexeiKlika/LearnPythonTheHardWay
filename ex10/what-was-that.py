56ec00e7a0adb6c1a6692c2765c5b1316822586e
Alexei Klika <AlexeiKlika@users.noreply.github.com>
2015-06-13 18:18:45 -0500

    Add exercises 10-12

---
+++ ex10/what-was-that.py
@@ -0,0 +1,31 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+print '''
+\\
+\'
+\"
+dd\b
+\f
+\n
+\N{0166}
+\r
+\ttest
+\u0147
+vertical tab
+\v
+vertical tab
+'''
