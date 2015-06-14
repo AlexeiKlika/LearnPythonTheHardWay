7e01005aed294243104f6f651a0ddb2b0dbb3dee
Alexei Klika <AlexeiKlika@users.noreply.github.com>
2015-06-14 12:58:33 -0500

    Add exercises 13-14

---
+++ ex13/combo.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, taters = argv
+
+type_of_taters = raw_input("How do you like your taters?! ")
+
+print "%s, Huh? Well I don't have time for that! I'll let you use my microwave..." % type_of_taters
---
+++ ex13/more-parameters.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, word1, word2, word3 = argv
+
+print "I have many things to says like: %s, %s, and %s!" % (word1,word2,word3)
